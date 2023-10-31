ab89bd49f025491e6e0f42183bce2181cc157555
trust <isaactrust447@gmail.com>
2023-10-31 20:38:56 +0100

    regex matching a string that starts with h ends with n and can have any single character in between

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -2,4 +2,4 @@
 #  regular expression matching a string
 #  that starts with "h" ends with "n"
 #  and can have any single character in between
-puts ARGV[0].scan(/^h[a-z]+n$/).join
+puts ARGV[0].scan(/h.n/).join
