fc63e104e0f6d57fcac73902325f7365b11bd843
trust <isaactrust447@gmail.com>
2024-02-29 02:16:24 +0100

    go

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""Accessing a REST API for todo lists of employees"""
-
+"""
+module to convert response to json file
+"""
+import json
 import requests
 import sys
 
-
 if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
+    emp_id = int(sys.argv[1])
+    url = f'https://jsonplaceholder.typicode.com/users/{emp_id}/todos'
+    usr_url = f'https://jsonplaceholder.typicode.com/users/{emp_id}'
 
-    response = requests.get(url)
-    username = response.json().get('username')
+    data = requests.get(url).json()
+    usr_data = requests.get(usr_url).json().get('username')
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
+    data_list = []
+    for info in data:
+        new_dict = {}
+        new_dict['task'] = info.get('title')
+        new_dict['completed'] = info.get('completed')
+        new_dict['username'] = usr_data
+        data_list.append(new_dict)
+    my_dict = {emp_id: data_list}
 
-    with open('{}.csv'.format(employeeId), 'w') as file:
-        for task in tasks:
-            file.write('"{}","{}","{}","{}"\n'
-                    .format(employeeId, username, task.get('completed'),
-                        task.get('title')))
+    with open(f'{emp_id}.json', 'w') as my_file:
+        json.dump(my_dict, my_file)
