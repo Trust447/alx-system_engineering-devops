11fc45f4da1ab7d601bea3c69656cb404d9ac3a2
trust <isaactrust447@gmail.com>
2024-03-06 07:29:42 +0100

    go

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    headers = {"User-Agent": 'Google Chrome Version 81.0.4044.129'}
+    user_agent = {"User-Agent": 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
     results = response.json()
